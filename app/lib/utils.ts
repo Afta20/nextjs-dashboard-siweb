2b8d6e6f26da57b999393a05881223e4b5a8f122
Afta20 <95036695+Afta20@users.noreply.github.com>
2026-04-29 16:08:13 +0700

    ch9

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -41,7 +41,7 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
   if (totalPages <= 7) {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
-
+  
   // If the current page is among the first 3 pages,
   // show the first 3, an ellipsis, and the last 2 pages.
   if (currentPage <= 3) {
